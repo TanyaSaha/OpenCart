4bfb98d9f49506a13564593ec32d74f59dbc125e
TanyaSaha <ravindra.kamisetty@gmail.com>
2023-10-28 19:12:39 +0530

    2nd Commit

---
+++ src/test/java/testCases/TC_003_SampleTest.java
@@ -0,0 +1,13 @@
+package testCases;
+
+import org.testng.annotations.Test;
+
+public class TC_003_SampleTest {
+	
+	@Test
+	public void method()
+	{
+		System.out.println("This is a test method");
+	}
+
+}
